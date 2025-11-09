dc32e4c66903f6762d27efc4ab53694cd542088e
Psidiropoulu <ps972@cam.ac.uk>
2025-11-09 10:48:51 +0000

    tests from CUED github repo added

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from Actuators.test_led import test_led
+from Actuators.test_input import test_input_poll
+from Actuators.test_motor import test_motor3
+from Actuators.test_actuator import test_actuator1
+from Sensors.test_tcs3472 import test_tcs3472
+from Sensors.test_vl53l0x import test_vl53l0x
+from Sensors.test_TMF8x01_get_distance import test_TMF8x01_get_distance
+from Sensors.test_STU_22L_IO_Mode import test_STU_22L_IO_Mode
+from Sensors.test_STU_22L_UART import test_STU_22L_UART
+
+print("Welcome to main.py!")
+
+# Uncomment the test to run
+# test_led()
+# test_led_pwm()
+# test_input_poll()
+# test_motor3()
+# test_tcs3472()
+# test_actuator1()
+# test_vl53l0x()
+# test_mfrc522()
+# test_TMF8x01_get_distance()
+# test_STU_22L_IO_Mode()
+# test_STU_22L_UART()
+# test_tiny_code_reader()
+
+print("main.py Done!")
