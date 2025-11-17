2a7b4711598dfa20cf2f2ce9ae0ccaa5daf83c77
Psidiropoulu <ps972@cam.ac.uk>
2025-11-17 10:23:25 +0000

    loop test added, turns a bit wiggly

--- src/Controller/map_navigation_v2.py
+++ src/Controller/map_navigation_v2.py
@@ -27,7 +27,7 @@ def move_forward(left, right, speed=60, dist_m=0.3):
     left.off(); right.off()
 
 
-def turn(left, right, rel_dir, speed=40, t=2.2):
+def turn(left, right, rel_dir, speed=40, t=0.8):
     # rel_dir: +1 = right 90°, -1 = left 90°, 2 = U-turn
     if rel_dir == +1:
         left.Forward(speed); right.Reverse(speed)
@@ -47,27 +47,24 @@ def read_sensor(pin_num):
 
 
 def line_follow(left, right):
-    base, turn_spd = 75, 60
-
+    base, turn_spd = 50, 40
     while True:
         s16, s17, s18, s19 = [read_sensor(i) for i in (8, 9, 10, 11)]
-        # print(f"Sensors: {s16} {s17} {s18} {s19}")
 
         if s17 == 1 and s18 == 1:
             left.Forward(base); right.Forward(base)
-        
-        if s17 == 1 and s18 == 0:
+        elif s17 == 0 and s18 == 1:
             left.Forward(turn_spd); right.Forward(base)
-        
-        if s17 == 0 and s18 == 1:
+        elif s17 == 1 and s18 == 0:
             left.Forward(base); right.Forward(turn_spd)
-        
-        if s16 == 1 or s19 == 1:
-            left.off()
-            right.off()
+        elif s16 == 1 or s19 == 1:
+            print("Possible junction detected")
+            left.off(); right.off()
             return "junction"
-
-        sleep(0.01)
+        else:
+            left.off(); right.off()
+            return "lost"
+        sleep(0.05)
 
 
 # ------------------ GRAPH NAVIGATION ------------------
@@ -77,26 +74,7 @@ maze_map = {
     0: {1: 0},
     1: {2: 3},
     2: {3: 2, 4: 3},
-    4: {5: 2, 6: 0},
-    6: {7: 0},
-    7: {8: 0},
-    8: {9: 0},
-    9: {10: 0},
-    10: {11: 0},
-    11: {12: 0},
-    12: {13: 0},
-    13: {14: 1},
-    14: {15: 1},
-    15: {16: 2},
-    16: {17: 2},
-    17: {18: 2},
-    18: {19: 2},
-    19: {20: 2},
-    20: {21: 2},
-    21: {22: 2},
-    22: {23: 2},
-    23: {24: 3},
-    24: {1: 3},
+    4: {5: 2, 6: 0}
 }
 
 def relative_turn(current_dir, new_dir):
@@ -109,29 +87,26 @@ def relative_turn(current_dir, new_dir):
 
 def navigate_path(path):
     left, right = Motor(4, 5), Motor(7, 6)
-    # direction = 0  # starting orientation (north)
-    direction = 3
-
-    try:
-        for i in range(len(path) - 1):
-            node, next_node = path[i], path[i + 1]
-            new_dir = maze_map[node][next_node]
-            rel = relative_turn(direction, new_dir)
-            print(f"At node {node}, going to {next_node}, rel_turn={rel}")
-            if rel != 0:
-                turn(left, right, rel)
-            move_forward(left, right, dist_m=0.1) # move forward to clear the junction
-            line_follow(left, right)
-            direction = new_dir
-    except KeyboardInterrupt:
-        left.off(); right.off()
+    direction = 0  # starting orientation (north)
+    for i in range(len(path) - 1):
+        node, next_node = path[i], path[i + 1]
+        new_dir = maze_map[node][next_node]
+        rel = relative_turn(direction, new_dir)
+        print(f"At node {node}, going to {next_node}, rel_turn={rel}")
+        if rel != 0:
+            turn(left, right, rel)
+        line_follow(left, right)
+        # move_forward(left, right)
+        # move_forward is the same as line_follow
+        direction = new_dir
     left.off(); right.off()
 
 
 # ------------------ MAIN ------------------
 
 if __name__ == "__main__":
-    path = [1, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]   # example path sequence through the maze
+    path = [1, 2, 4, 5]   # example path sequence through the maze
     path_back = path[::-1]  # [5, 4, 2, 1, 0]
     navigate_path(path)
     navigate_path(path_back)
+    # need to reverse line following when going in the back direction and relative directions shoild
